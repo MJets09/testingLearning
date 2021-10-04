ab5aad388555af2a922200a624079d00cc334a11
MJets09 <mthaoqk@gmail.com>
2021-10-04 14:51:08 -0500

    refreshers

---
+++ assests/js/js.js
@@ -0,0 +1,21 @@
+let firstName = 'Joe';
+
+
+console.log(`My name is ${firstName}`);
+
+let counter = document.querySelector('h1');
+let btn = document.querySelector('button');
+
+let plusOne = x => {
+    console.log(x);
+}
+
+plusOne(9);
+
+let doge = {
+    type: 'shiba',
+    age: 4,
+    color: 'orange'
+}
+
+console.log(doge.color);
