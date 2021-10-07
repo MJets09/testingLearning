df6079da5537bd53e9339a8416acfbf5c421064d
MJets09 <mthaoqk@gmail.com>
2021-10-06 20:31:52 -0500

    testing with map,filter,forEach

--- assests/js/js.js
+++ assests/js/js.js
@@ -10,7 +10,7 @@ let counter = 1
 function plusOne() {
     document.querySelector('p').innerHTML = counter;
     counter++
-    if (counter === 10) {
+    if (counter % 10 == 0) {
         alert(`Did it, counter is now at ${counter}`)
     }
 }
@@ -45,9 +45,9 @@ let asL = document.querySelector('#ASL');
 
 sabo.innerHTML = ASL[1];
 
-for (let i = 0; i < ASL.length; i++) {
-    asL.innerHTML += ASL[i] + ' ';
-}
+// for (let i = 0; i < ASL.length; i++) {
+//     asL.innerHTML += ASL[i] + ' ';
+// }
 
 let port = document.querySelector('#ace');
 
@@ -59,3 +59,31 @@ function autoTen() {
 let aceBtn = document.querySelector('#aka');
 
 aceBtn.addEventListener('click', autoTen);
+
+document.querySelector('select').onchange = function() {
+    document.querySelector('#Sabo').style.color = this.value;
+}
+
+const strawHats = ['Zoro', 'Nami', 'Sanji', 'Usopp', 'Choppa', 'Robin'];
+
+let ages = [33, 55, 11, 22, 44, 15, 69, 74, 81, 24];
+
+let brothers = (a, s, l) => console.log(a, s, l);
+
+
+brothers('Ace', 'Sabo', 'Luffy');
+
+let canDrink = ages.filter(x => x >= 21);
+
+console.log(canDrink);
+
+let Wu = [
+    { name: 'Sun Jian', weapon: 'Sword' },
+    { name: 'Zhou Yu', weapon: 'Bo Staff' },
+    { name: 'Zhou Tai', weapon: 'Sword' },
+    { name: 'SunShangXiang', weapon: 'Chakrams' }
+];
+
+let swordUser = Wu.filter(hero => hero.weapon === 'Bo Staff');
+
+console.log(swordUser);
