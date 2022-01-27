5580bdd8b49ef4c016cf5d3ea93b20ba7a686a9c
MJets09 <mthaoqk@gmail.com>
2022-01-26 20:30:34 -0600

    Small JS battleship project, more understanding of how pseudocode works and learning to use flowcharts

---
+++ battleship/js.js
@@ -0,0 +1,30 @@
+// User starts the Gamepad(Game places a battleship on a random place on the grid)
+// Repeat the following until the battleship is sink(Prompt user for a guess(2,0, etc.))
+// Game finishes, give user a rating based on the number of guess
+
+//Get user score
+//Does it hit?
+//If not, start guess again
+//If so mark hit
+//If all 3 spots hit
+//Game over
+
+
+let location1;
+let location2;
+let location3;
+let userGuess = prompt('Coordinates?')
+let guess = userGuess;
+let hit = 0;
+let guesses = 0;
+let isSunk = false;
+
+while (isSunk == false) {
+    guess
+    if (userGuess == location1, location2, location3) {
+        hit++
+        guesses++
+    } else {
+        guess
+    }
+}
