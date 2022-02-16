60ab0b9fa934351342c6d23366009cf66490b4cb
MJets09 <mthaoqk@gmail.com>
2022-02-16 12:04:33 -0600

    Small program dealing with Battleship logic for Js. JS testing

--- battleship/js.js
+++ battleship/js.js
@@ -10,33 +10,34 @@
 //Game over
 
 
-let location1 = Math.floor(Math.random() * 3) + 1;;
-let location2;
-let location3;
-let userGuess = prompt('Coordinates?')
-let guess = userGuess;
+let location1 = Math.floor(Math.random() * 7);
+let location2 = Math.floor(Math.random() * 7);
+let location3 = Math.floor(Math.random() * 7);
 let hit = 0;
 let guesses = 0;
 let isSunk = false;
 
-// while (isSunk == false) {
-//     guess
-//     if (guess == location1, location2, location3) {
-//         alert('Got me')
-//         hit++
-//         guesses++
-//         guess
-//     } else if (guess != location1, location2, location3) {
-//         alert('Nope, nothinggg')
-//         guess++
-//         guess
-//     }
-// }
-
-
-do {
-    guess
-}
-while (location1 != guess)
-
 console.log(location1)
+console.log(location2)
+console.log(location3)
+
+while (isSunk == false) {
+    let userGuess = prompt('Coordinates?')
+    if (userGuess < 0 || userGuess > 6) {
+        alert('Only numbers between 0-6');
+    } else {
+        guesses++
+        if (userGuess == location1 || userGuess == location2 || userGuess == location3) {
+            hit++
+            // alert('Hit!');
+            if (hit == 3) {
+                isSunk = true;
+                alert('Sinked it')
+            }
+        } else {
+            alert('Missed')
+        }
+    }
+}
+
+var stats = alert('You took ' + guesses + ' tries to sank the battleship')
