4f32f23aca37c230208edb69164227491b150137
MJets09 <mthaoqk@gmail.com>
2022-04-26 22:07:14 -0500

    Testing API

--- 3API/js.js
+++ 3API/js.js
@@ -1,7 +1,9 @@
 document.querySelector('button').addEventListener('click', getPoke)
 
-function cap(str) {
-    str.charAt(0).toUpperCase() + str.slice(1);
+function removeElements(elements) {
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].parentNode.removeChild(elements[i]);
+    }
 }
 
 function getPoke() {
@@ -12,9 +14,12 @@ function getPoke() {
         .then(res => res.json())
         .then(data => {
             console.log(data)
+
+
             document.querySelector('img').src = data.sprites.front_default;
             document.querySelector('span').innerText = data.species.name;
 
+            removeElements(document.querySelectorAll('li'));
 
             data.abilities.forEach(x => {
                 let newLi = document.createElement('li');
@@ -31,6 +36,41 @@ function getPoke() {
         .catch(err => {
             console.log(`error ${err}`)
         })
+}
 
+document.querySelector('img').addEventListener('click', backSprite);
+
+function backSprite() {
+
+    let userChoice = document.querySelector('input').value.toLocaleLowerCase()
+    let url = `https://pokeapi.co/api/v2/pokemon/${userChoice}`
+
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+
+
+            document.querySelector('img').src = data.sprites.back_default;
+            document.querySelector('span').innerText = data.species.name;
+
+            removeElements(document.querySelectorAll('li'));
+
+            data.abilities.forEach(x => {
+                let newLi = document.createElement('li');
+                let newP = document.createElement('p')
+
+                newP.innerText = x.ability.name
+                console.log(newP)
+                newLi.appendChild(newP)
+
+                document.getElementById('abilities').appendChild(newLi)
+            })
+
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
 
 }
