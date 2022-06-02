f340a2cf97cb9062683ab2a3d425491b9e710039
Meng <mthaoqk@gmail.com>
2022-06-01 20:01:22 -0500

    Figureout connection issue mongoDB

--- serverSide/server.js
+++ serverSide/server.js
@@ -1,6 +1,20 @@
 
 const express = require('express')
 const app = express()
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+
+MongoClient.connect('mongodb-connection-string', (err, client) => {
+    MongoClient.connect(connectionString, (err, client) => {
+        if (err) return console.error(err)
+        console.log('Connected to Database')
+        'mongodb+srv://mthao09:<3169kaT>@cluster0.lojrg.mongodb.net/?retryWrites=true&w=majority'
+      })
+  })
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.listen(3500, () => {
     console.log('On port 3500')
@@ -8,4 +22,8 @@ app.listen(3500, () => {
 
 app.get('/', (req,res) =>{
     res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req,res)=>{
+    res.send(console.log(req.body))
 })
